ea5153bcc3af2fbe1c62ed1a8b6ff6ecd12ee1a6
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2022-02-26 09:32:55 +0100

    Round display of initiative value up

--- module/helpers/handlebars.mjs
+++ module/helpers/handlebars.mjs
@@ -3,47 +3,51 @@
 /* -------------------------------------------- */
 export class registerHelpers {
     static init() {
-        
-      Handlebars.registerHelper('concat', function() {
-          var outStr = '';
-          for (var arg in arguments) {
-            if (typeof arguments[arg] != 'object') {
-              outStr += arguments[arg];
+
+        Handlebars.registerHelper('concat', function () {
+            var outStr = '';
+            for (var arg in arguments) {
+                if (typeof arguments[arg] != 'object') {
+                    outStr += arguments[arg];
+                }
             }
-          }
-          return outStr;
+            return outStr;
         });
-    
-      Handlebars.registerHelper('toLowerCase', function(str) {
-          return str.toLowerCase();
+
+        Handlebars.registerHelper('toLowerCase', function (str) {
+            return str.toLowerCase();
         });
-  
-      Handlebars.registerHelper('ifCond', function (arg1, operator, arg2, options) {
-  
-          switch (operator) {
-              case '==':
-                  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-              case '===':
-                  return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
-              case '!=':
-                  return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
-              case '!==':
-                  return (arg1 !== arg2) ? options.fn(this) : options.inverse(this);
-              case '<':
-                  return (arg1 < arg2) ? options.fn(this) : options.inverse(this);
-              case '<=':
-                  return (arg1 <= arg2) ? options.fn(this) : options.inverse(this);
-              case '>':
-                  return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
-              case '>=':
-                  return (arg1 >= arg2) ? options.fn(this) : options.inverse(this);
-              case '&&':
-                  return (arg1 && arg2) ? options.fn(this) : options.inverse(this);
-              case '||':
-                  return (arg1 || arg2) ? options.fn(this) : options.inverse(this);
-              default:
-                  return options.inverse(this);
-          }
-      });
+
+        Handlebars.registerHelper('roundUp', function (int) {
+            return Math.ceil(int);
+        });
+
+        Handlebars.registerHelper('ifCond', function (arg1, operator, arg2, options) {
+
+            switch (operator) {
+                case '==':
+                    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+                case '===':
+                    return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+                case '!=':
+                    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+                case '!==':
+                    return (arg1 !== arg2) ? options.fn(this) : options.inverse(this);
+                case '<':
+                    return (arg1 < arg2) ? options.fn(this) : options.inverse(this);
+                case '<=':
+                    return (arg1 <= arg2) ? options.fn(this) : options.inverse(this);
+                case '>':
+                    return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
+                case '>=':
+                    return (arg1 >= arg2) ? options.fn(this) : options.inverse(this);
+                case '&&':
+                    return (arg1 && arg2) ? options.fn(this) : options.inverse(this);
+                case '||':
+                    return (arg1 || arg2) ? options.fn(this) : options.inverse(this);
+                default:
+                    return options.inverse(this);
+            }
+        });
     }
-  }
+}
