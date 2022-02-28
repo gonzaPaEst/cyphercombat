cbde7195d077c2b4cad02ad9d8230698cf4cba93
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2022-02-28 09:21:59 +0100

    Fixed highlight to active token instead of actor

--- module/helpers/combat.mjs
+++ module/helpers/combat.mjs
@@ -245,7 +245,7 @@ export class CypherCombatSidebar {
         combatant.isObserver = (combatant.actor.permission == 2) ? true : false;
 
         // Determine if combatant is active
-        combatant.active = (combatant.data.actorId == game.combat.combatant.data.actorId) ? true : false;
+        combatant.active = (combatant.data.tokenId == game.combat.combatant.data.tokenId) ? true : false;
       }
       // Append actors
       combatants.push(combatant)
